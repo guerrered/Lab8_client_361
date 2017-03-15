d2745a0e9bf392c972fbd447b0abb2c766227ed9
guerrered <guerrered8@gmail.com>
2017-03-15 13:40:33 -0500

    employee

--- src/Employee.java
+++ src/Employee.java
@@ -5,12 +5,15 @@ public class Employee {
 	String firstName;
 	String Phone;
 	String Department;
-	
-	public Employee(String LastName, String FirstName, String Phone, String Department){
+	String Gender;
+	String Title;
+	public Employee(String LastName, String FirstName, String Phone, String Department,String Gender, String Title){
 		lastName = LastName;
 		firstName = FirstName;
 		this.Phone = Phone;
 		this.Department = Department;
+		this.Gender = Gender;
+		this.Title = Title;
 	}
 	
 	public String toString(){
