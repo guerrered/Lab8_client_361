03be566387ab5d667cf1270cf12c1d4db711327e
John Krause <jekrause@uwm.edu>
2017-03-15 14:34:30 -0500

    client

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+import java.awt.event.ActionEvent;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -11,7 +12,9 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class Client {
-
+	static GUI gui;
+	String data;
+	static DataOutputStream out;
 	public static void main(String[] args) {
 		try {
 			System.out.println("in the client");
@@ -24,18 +27,21 @@ public class Client {
 			conn.setRequestMethod("POST");
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
-			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-
+			//DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+			out = new DataOutputStream(conn.getOutputStream());
+			
 			// build a string that contains JSON from console
-			String content = "";
-			content = getJSON();
+			//String content = "";
+			//content = getJSON();
+			
+			gui = new GUI();
 
 			// write out string to output buffer for message
-			out.writeBytes(content);
-			out.flush();
-			out.close();
+//			out.writeBytes(content);
+//			out.flush();
+//			out.close();
 
-			System.out.println("Done sent to server");
+//			System.out.println("Done sent to server");
 
 			InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
 
@@ -54,17 +60,60 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-
-	private static String getJSON() {
-
-		ArrayList<Employee> em = new ArrayList<>();
-		em.add(new Employee("Eric", "Fritz", "Computer Science", "1"));
-		em.add(new Employee("Tanawat", "Khun", "Computer Science", "2"));
-		em.add(new Employee("Foo", "Bar", "Baz", "3"));
-		em.add(new Employee("Donald", "Duck", "Disney Animals", "4"));
-		Gson g = new Gson();
-		String json = g.toJson(em);
-		return json;
+	
+	public void actionPerformed(ActionEvent e){
+		//add
+		if(e.getSource()==gui.buttonAdd){
+			data =add(gui.getLastName(), gui.getFirstName(), gui.getPhone(), gui.getDept(), gui.getGender(), gui.getTitle());
+			out.writeBytes(data);
+			out.flush();
+			out.close();
+			System.out.println("Done sent to server");
+		}
+		//clear
+		else if(e.getSource() == gui.buttonClear){
+			data = "CLEAR";
+			out.writeBytes(data);
+			out.flush();
+			out.close();
+			System.out.println("Done sent to server");
+		}
+		//print
+		else{
+			data = "PRINT";
+			out.writeBytes(data);
+			out.flush();
+			out.close();
+			System.out.println("Done sent to server");
+		}
 	}
+	
+	public String add(String LastName, String FirstName, String Phone, String Department,String Gender, String Title){
+		Employee employee = new Employee(LastName, FirstName, Phone, Department, Gender, Title);
+		Gson g = new Gson();
+		String json = g.toJson(employee);
+		String addS = "ADD";
+		addS+=json;
+		
+		return addS;
+		
+	}
+	
+	public void close(){
+		
+	}
+
+//	private static String getJSON() {
+//
+//		ArrayList<Employee> em = new ArrayList<>();
+//		em.add(new Employee("Eric", "Fritz", "Computer Science", "1"));
+//		em.add(new Employee("Tanawat", "Khun", "Computer Science", "2"));
+//		em.add(new Employee("Foo", "Bar", "Baz", "3"));
+//		em.add(new Employee("Donald", "Duck", "Disney Animals", "4"));
+//		Employee employee = new Employee(null, null, null, null, null, null);
+//		Gson g = new Gson();
+//		String json = g.toJson(employee);
+//		return json;
+//	}
 
 }
